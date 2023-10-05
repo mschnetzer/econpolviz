2b9dc801fcf7cdbb220a8cf583fc9d8489956dd2
Matthias Schnetzer <mschnetz@wu.ac.at>
2023-10-05 09:44:34 +0200

    Built site for gh-pages

---
+++ data/code/02_visualization.R
@@ -0,0 +1,121 @@
+###########################
+## 03 DATA VISUALIZATION ##
+###########################
+
+# Load general packages
+library(tidyverse)
+library(lubridate) # for dates and times
+library(scales) # for scale layouts (breaks and labels)
+
+##############
+## PENGUINS ##
+##############
+
+# Load and assign data
+library(palmerpenguins)
+data <- penguins
+
+# Take brief look
+head(data)
+
+# Calculate median and standard deviation for bill length and depth
+data_summary <- data |>
+  group_by(species) |>
+  summarise(across(c(bill_length_mm, bill_depth_mm),
+                   list(median = ~median(., na.rm = TRUE), 
+                        sd = ~sd(., na.rm = TRUE))))
+
+# Scatter plot with error bars by species
+# Idea by Cedric Scherer: https://www.behance.net/gallery/101517403/Bill-Dimensions-of-Penguins
+data |> ggplot(aes(x = bill_length_mm, 
+                   y = bill_depth_mm, 
+                   color = species)) +
+  # Error bars at the median with the standard deviations
+  # Attention: we take other data here with new aesthetics, so inherit.aes = F
+  geom_errorbar(
+    data = data_summary,
+    aes(x = bill_length_mm_median,
+        ymin = bill_depth_mm_median - bill_depth_mm_sd,
+        ymax = bill_depth_mm_median + bill_depth_mm_sd,
+        color = species,
+        color = after_scale(colorspace::darken(color, .2, space = "combined"))
+    ),
+    inherit.aes = F, width = .8, linewidth = .8
+  ) +
+  geom_errorbar(
+    data = data_summary,
+    aes(y = bill_depth_mm_median,
+        xmin = bill_length_mm_median - bill_length_mm_sd,
+        xmax = bill_length_mm_median + bill_length_mm_sd,
+        color = species,
+        color = after_scale(colorspace::darken(color, .2, space = "combined"))
+    ),
+    inherit.aes = F, width = .8, linewidth = .8
+  ) +
+  geom_point(size = 1.5, alpha = 0.5) +
+  scale_color_manual(name = NULL,
+                     values = MetBrewer::met.brewer("Lakota")) +
+  scale_x_continuous(labels = scales::number_format(suffix="mm")) +
+  scale_y_continuous(labels = scales::number_format(suffix="mm", accuracy = 1)) +
+  # Add labels in the plot rather than in legend
+  annotate("text", x = c(34.7, 55.7, 50.7), y = c(20.7, 19, 13.6), 
+           color = MetBrewer::met.brewer("Lakota")[1:3], 
+           label = c("Adélie","Chinstrap","Gentoo"), fontface = "bold", size = 4) +
+  labs(x = "Bill length", y = "Bill depth",
+       title = "Penguins are awesome",
+       subtitle = "Depth and length of bills") +
+  theme_minimal() +
+  theme(legend.position = "none",
+        plot.title.position = "plot",
+        plot.title = element_text(size = 15),
+        plot.subtitle = element_text(size = 13),
+        panel.grid.minor = element_blank())
+  
+
+
+
+##############
+## EUROSTAT ##
+##############
+
+library(eurostat)
+
+# Get monthly retail sales data from Eurostat
+retdata <- get_eurostat("sts_trtu_m", filters = list(indic_bt = "TOVV", s_adj = "CA", unit = "PCH_SM", nace_r2 = "G47", geo = c("AT","DE","FR","ES","IT","PT")), time_format = "date", select_time = "M", type = "code")
+
+# Alternatively, load local RData file
+# load("03_visualization.RData")
+
+# Subset of latest 10 months and edit labels
+plotdat <- retdata |> 
+  slice_max(time, n = 10, by = geo) |> 
+  label_eurostat(dic = "geo", lang = "en") |> 
+  mutate(geo = case_when(str_detect(geo, "Germany") ~ "Germany", TRUE ~ geo))
+
+plotdat |> 
+  ggplot(aes(x = time, y = values)) +
+  # Different colors for positive and negative values (with ifelse statement)
+  geom_segment(aes(y = 0, yend = values, xend = time, 
+                   color = ifelse(values < 0, "green", "red"))) +
+  # Horizontal zero line
+  geom_hline(yintercept = 0) +
+  geom_point(aes(color = ifelse(values < 0, "green", "red"))) + 
+  # Create small multiples by country
+  facet_wrap(~geo) +
+  # Axis labels as percentages
+  scale_y_continuous(labels = scales::percent_format(scale = 1)) +
+  scale_x_date(date_labels = "%b %y", date_breaks = "3 months") +
+  theme_minimal() +
+  labs(x = "", y = "",
+       title = "How did retail sales change in the last 10 months?",
+       subtitle = "Percentage change compared to the same period in previous year",
+       caption = "Note: Data is calendar but not seasonally adjusted\n Data: Eurostat. Figure: @matschnetzer") +
+  theme(legend.position = "none",
+        plot.subtitle = element_text(margin = margin(t = 5, b = 10, unit = "pt")),
+        plot.caption = element_text(size = 6),
+        axis.text.y = element_text(size = 8),
+        axis.text.x = element_text(size = 7),
+        panel.spacing.x = unit(2, "lines"),
+        strip.text = element_text(size = 10))
+
+ggsave(filename = "plots/retail_sales.png", dpi = 320, width = 8, height = 4)
---
+++ data/code/03_geometries.R
@@ -0,0 +1,160 @@
+############################
+## 04 GROWTH · GEOMETRIES ##
+############################
+
+# load packages
+library(tidyverse)
+library(eurostat)
+library(MetBrewer)
+
+# Search datasets for GDP (per capita)
+search_eurostat("GDP") |> View()
+
+# get GDPpc data from Eurostat
+rawgdp <- get_eurostat("nama_10_pc", time_format = "num", type = "label", filters = list(geo = c("AT","FR","IT","DE","ES")))
+
+# Alternatively, load local RData file
+# load("04_growth.RData")
+
+View(rawgdp)
+
+gdp <- rawgdp |> 
+  filter(unit %in% c("Chain linked volumes (2010), euro per capita",
+            "Chain linked volumes, percentage change on previous period, per capita"),
+         na_item == "Gross domestic product at market prices") |>  
+  mutate(geo = ifelse(str_detect(geo, "Germany"), "Germany", geo)) |> 
+  filter(time %in% 2000:2021) |> 
+  drop_na()
+
+
+## Let's try different geometries
+# 1. Line plot with evolution of GDPpc
+gdp |> filter(unit == "Chain linked volumes (2010), euro per capita") |> 
+  ggplot(aes(x = time, y = values, group = geo, color = geo)) +
+  geom_line(linewidth = 1) +
+  scale_color_manual(name = NULL, values = met.brewer("Juarez")) +
+  scale_y_continuous(labels = scales::number_format(prefix = "€", big.mark = ",")) +
+  labs(x = NULL, y = NULL,  title = "Evolution of GDP 2000-2021", 
+       subtitle = "GDP per capita in €") +
+  theme_minimal() +
+  theme(panel.grid.minor = element_blank(),
+        legend.position = "bottom")
+ggsave("plots/gdp_evolution.png", width = 6, height = 4, dpi = 320)
+
+# Annotation within the plot; for casual style, try "stat = 'smooth'"
+library(geomtextpath)
+gdp |> filter(unit == "Chain linked volumes (2010), euro per capita") |> 
+  ggplot(aes(x = time, y = values, group = geo, color = geo)) +
+  geomtextpath::geom_textline(aes(label = geo), hjust = 0.75, vjust = 0.5, #stat = "smooth",
+                              size = 3, fontface = "bold", linewidth = 0.8) +
+  scale_color_manual(values = met.brewer("Juarez")) +
+  scale_y_continuous(labels = scales::number_format(prefix = "€", big.mark = ",")) +
+  labs(x = NULL, y = NULL, title = "Evolution of GDP 2000-2021", 
+       subtitle = "GDP per capita in €") +
+  theme_minimal() +
+  theme(legend.position = "none")
+ggsave("plots/gdp_evolution_label.png", width = 6, height = 4, dpi = 320)
+  
+
+# 2. Create base year for better comparability of evolution & reorder countries for legend
+gdpbase <- gdp |> filter(unit == "Chain linked volumes (2010), euro per capita") |> 
+  mutate(base = values / values[time == 2000] * 100, .by = geo) |> 
+  mutate(geo = fct_reorder2(geo, time, base))
+
+gdpbase |> ggplot(aes(x = time, y = base, group = geo, color = geo)) +
+  geom_hline(yintercept = 100, color = "black", linewidth = 0.5) +
+  geom_line(linewidth = 0.8) +
+  geom_label(aes(label = round(base,0)), data = gdpbase |> slice_max(time, n=1), 
+            hjust = 0.5, size = 2.5, show.legend = F) +
+  scale_color_manual(name = NULL, values = met.brewer("Juarez")) +
+  scale_x_continuous(expand = c(0.07,0)) +
+  labs(x = NULL, y = NULL, title = "Evolution of GDP", 
+       subtitle = "GDP per capita (2000 = 100)") +
+  theme_minimal() +
+  theme(legend.position = "right")
+
+
+# 3. Boxplots with growth rates
+gdpgrowth <- gdp |> filter(str_detect(unit, "percentage"))
+
+gdpgrowth |> ggplot(aes(x = geo, y = values, group = geo, color = geo)) +
+  geom_boxplot(outlier.alpha = 0) +
+  geom_jitter(width = 0.2, alpha = 0.8) +
+  scale_color_manual(values = met.brewer("Juarez")) +
+  scale_y_continuous(labels = scales::percent_format(scale = 1)) +
+  labs(x = NULL, y = NULL, title = "Dispersion of GDP growth rates",
+       subtitle = "Growth rates of per capita GDP between 2000 and 2021") +
+  theme_minimal() +
+  theme(panel.grid.major.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        legend.position = "none")
+
+
+# 4. Facets with bar charts
+gdpgrowth |> ggplot(aes(x = time, y = values, fill = geo)) + 
+  geom_bar(stat = "identity") +
+  facet_wrap(~geo) +
+  scale_fill_manual(values = met.brewer("Juarez")) +
+  scale_y_continuous(labels = scales::percent_format(scale = 1)) +
+  labs(x = NULL, y = NULL, title = "GDP growth rates from 2000 to 2021") +
+  theme_minimal() +
+  theme(legend.position = "none",
+        panel.grid.minor = element_blank(),
+        plot.title.position = "plot")
+
+# 5. Lollipop chart 
+gdpgrowth |> ggplot(aes(x = time, y = values)) + 
+  geom_segment(aes(xend = time, yend = 0), color = "gray90", size = 2.5) + 
+  geom_hline(yintercept = 0, color = "black", size = 0.3) + 
+  geom_point(aes(color = ifelse(values > 0, "pos", "neg")), size = 2.5) +
+  facet_wrap(~geo) +
+  scale_color_manual(values = c("pos" = "darkolivegreen3", "neg" = "red")) +
+  scale_y_continuous(labels = scales::percent_format(scale = 1)) +
+  labs(x = NULL, y = NULL, title = "GDP growth rates from 2000 to 2021") +
+  theme_minimal(base_size = 16) +
+  theme(legend.position = "none",
+        panel.border = element_rect(fill = "transparent"),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_line(linewidth = 0.3),
+        plot.title = element_text(hjust = .5, size = 20))
+
+# 6. Bump plot
+#remotes::install_github('rensa/ggflags')
+library(ggbump)
+library(ggflags)
+library(futurevisions)
+library(colorspace)
+library(countrycode)
+
+growthrank <- gdpgrowth |> 
+  mutate(rank = rank(-values, ties.method = "random"), .by = time) |> 
+  filter(time >= 2015) |> 
+  left_join(countrycode::codelist |> select(country.name.en, iso2c),
+            by = c("geo" = "country.name.en"))
+
+growthrank |> 
+  ggplot(aes(x = time, y = rank, color = geo)) +
+  geom_point(size = 4) +
+  geom_bump(linewidth = 2) +
+  geom_text(data = growthrank |> slice_max(time, n=1),
+            aes(x = time + 0.2, label = geo), size = 4.5, hjust = 0, fontface = "bold") +
+  geom_flag(data = growthrank |> slice_min(time, n=1), 
+            aes(x = 2015, y = rank, country = tolower(iso2c)), size = 8) +
+  scale_color_manual(values = lighten(futurevisions("mars"), .3)) +
+  scale_size_continuous(range = c(2, 6)) +
+  scale_x_continuous(breaks = seq(2015, 2021, 2), limits = c(2015, 2021.7)) +
+  scale_y_reverse() +
+  labs(x = NULL, y = NULL,
+       title = toupper("Ranking of GDP per capita growth rates")) +
+  theme_minimal() +
+  theme(plot.background = element_rect(fill = "black", color = NA),
+        plot.title = element_text(size = 20, hjust = 0.5, margin = margin(b = 1, unit = "lines")),
+        plot.subtitle = element_text(hjust = 0.5, size = 11, margin = margin(b=15, unit="pt")),
+        plot.title.position = "plot",
+        legend.position = "none",
+        panel.grid = element_blank(),
+        text = element_text(color = "white"),
+        axis.text.y = element_blank(),
+        axis.text.x = element_text(color = "white", size = 10))
+
+ggsave("plots/gdpbump.png", width = 8, height = 4, dpi = 320) 
